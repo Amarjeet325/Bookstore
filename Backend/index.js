e05c1b86633c4878a2cbb4bce0a4419138b0b8c1
Amarjeet patel <130923139+Amarjeet325@users.noreply.github.com>
2024-11-18 14:10:11 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,25 +9,27 @@ import userRoute from "./route/user.route.js";
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+// connect to mongoDB---->
+const PORT = process.env.PORT || 3001;
 const URI = process.env.MongoDBURI;
 
-// connect to mongoDB
 try {
     mongoose.connect(URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    console.log("Connected to mongoDB");
+    console.log("Connected to mongoDB !!");
 } catch (error) {
     console.log("Error: ", error);
 }
 
-// defining routes
+// defining routes-------
+
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
